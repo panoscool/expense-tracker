f1a4d63bcd865945885715269dfd0ba1eafa56bd
Panos K <lkullolli@gmail.com>
2022-06-24 11:14:51 +0400

    Removed unnecessary type from next link component

--- components/shared/next-link.tsx
+++ components/shared/next-link.tsx
@@ -1,21 +1,13 @@
-import Link, { LinkProps } from 'next/link';
 import MuiLink from '@mui/material/Link';
-import Button from '@mui/material/Button';
+import Link, { LinkProps } from 'next/link';
 
 type NextLinkProps = {
   href: LinkProps['href'];
   children: React.ReactNode;
-  type?: 'button';
   style?: React.CSSProperties;
 };
 
-const NextLink: React.FC<NextLinkProps> = ({ type, href, children, ...props }) => {
-  if (type === 'button') {
-    <Link href={href} passHref>
-      <Button {...props}>{children}</Button>
-    </Link>;
-  }
-
+const NextLink: React.FC<NextLinkProps> = ({ href, children, ...props }) => {
   return (
     <Link href={href} passHref>
       <MuiLink {...props}>{children}</MuiLink>
